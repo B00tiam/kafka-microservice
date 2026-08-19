642dc8701469120a4f8a25e8056839e493f60bfa
b00tim <hulk996r@yahoo.com>
2026-08-19 19:34:56 +0200

    Update domain & repo layers' code in order-service

--- order-service/src/main/java/self/dev/order/domain/Order.java
+++ order-service/src/main/java/self/dev/order/domain/Order.java
@@ -1,7 +1,88 @@
 package self.dev.order.domain;
 
+import self.dev.domain.enums.OrderStatus;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "orders")
 public class Order {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
 
+    @Column(nullable = false)
+    private String customerId;
+
+    @Column(nullable = false)
+    private String productId;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private Double amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    // temporarily not used timestamp
+    private String reason;
+
+    // Funcs:
+    protected Order() {
+        // JPA needs a default constructor
+    }
+
+    public Order(
+            String customerId,
+            String productId,
+            Integer quantity,
+            Double amount) {
+        this.customerId = customerId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.amount = amount;
+        this.status = OrderStatus.CREATED;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
 
 }
--- order-service/src/main/java/self/dev/order/repository/OrderRepository.java
+++ order-service/src/main/java/self/dev/order/repository/OrderRepository.java
@@ -1,4 +1,9 @@
 package self.dev.order.repository;
 
-public interface OrderRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import self.dev.order.domain.Order;
+
+
+// no need for write / override methods, JpaRepository provides all the necessary CRUD operations
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
