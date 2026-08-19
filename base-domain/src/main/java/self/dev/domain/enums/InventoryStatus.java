ce12162b13a8f9601c06ea9bf8e7bec3ee3ac21d
b00tim <hulk996r@yahoo.com>
2026-08-19 23:59:13 +0200

    Revise grammar error of InventoryStatus.java & PaymentStatus.java in base-domain

--- base-domain/src/main/java/self/dev/domain/enums/InventoryStatus.java
+++ base-domain/src/main/java/self/dev/domain/enums/InventoryStatus.java
@@ -2,9 +2,9 @@ package self.dev.domain.enums;
 
 public enum InventoryStatus {
     // 3 status: success, failed, pending
-    RESERVTION_PENDING,
-    RESERVTION_SUCCESS,
-    RESERVTION_FAILED,
+    RESERVATION_PENDING,
+    RESERVATION_SUCCESS,
+    RESERVATION_FAILED,
 
     // final check
     CONFIRMED,
