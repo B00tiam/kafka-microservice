475c0a07e514c2366c98462820620b67b61de3d5
b00tim <hulk996r@yahoo.com>
2026-08-28 10:48:53 +0200

    Update domain code in notification-service

--- notification-service/src/main/java/self/dev/notification/domain/Notification.java
+++ notification-service/src/main/java/self/dev/notification/domain/Notification.java
@@ -1,4 +1,75 @@
 package self.dev.notification.domain;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import self.dev.domain.enums.NotificationTypes;
+
+
+@Entity
+@Table(name = "notifications")
 public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long notificationId;
+
+    @Column(nullable = false, unique = true)
+    private Long orderId;
+
+    @Column(nullable = false)
+    private String customerId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private NotificationTypes type;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    // Funcs:
+    protected Notification() {
+        // JPA needs a default constructor
+    }
+
+    public Notification(
+            Long orderId,
+            String customerId,
+            NotificationTypes type,
+            String message) {
+
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.type = type;
+        this.message = message;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    // getters
+    public Long getNotificationId() {
+        return notificationId;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public NotificationTypes getType() {
+        return type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 }
